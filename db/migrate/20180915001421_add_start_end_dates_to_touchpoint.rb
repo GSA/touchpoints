5cce7b1474e9a349bd911db0a04e1a578d5c8a9f
Ryan Wold <ryan.wold@gsa.gov>
2018-09-20 16:34:34 -0700

    remove migrations
    * first installs will be based on schema.rb
    * migrations will be required when we're coordinating changes to the data model across instances of the app

--- db/migrate/20180915001421_add_start_end_dates_to_touchpoint.rb
+++
@@ -1,6 +0,0 @@
-class AddStartEndDatesToTouchpoint < ActiveRecord::Migration[5.2]
-  def change
-    add_column :touchpoints, :start_date, :datetime
-    add_column :touchpoints, :end_date, :datetime
-  end
-end
