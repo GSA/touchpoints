08b98991ebbe4825a6908bf2109cb1a7ad1114f0
Ryan Wold <ryan.wold@gsa.gov>
2018-09-20 16:30:52 -0700

    add an example page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,8 @@
 class SiteController < ApplicationController
   def index
   end
+
+  def example
+    render layout: false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :touchpoints
   devise_for :users
-  get 'site/index'
+  get "example", to: "site#example", as: :example
   root to: "site#index"
 end
