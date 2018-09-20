f8dbeb1e65cef3c62831e64f51c6d3fc152c174d
Ryan Wold <ryan.wold@gsa.gov>
2018-09-20 16:30:06 -0700

    add dotenv-rails to manage environment variables
    * to support 12-factor app architecture

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load if Rails.env.development? || Rails.env.test?
+
 module Engage
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
