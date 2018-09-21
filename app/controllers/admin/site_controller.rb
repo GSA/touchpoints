c856cfcd7a4e9d88acbc48002403592079539b03
Ryan Wold <ryan.wold@gsa.gov>
2018-09-21 15:44:12 -0700

    basic list of GTM settings for /admin config/troubleshooting purposes

---
+++ app/controllers/admin/site_controller.rb
@@ -0,0 +1,8 @@
+class Admin::SiteController < ApplicationController
+  def index
+    @google_api = GoogleApi.new
+    @account = @google_api.get_account(account_id: 4172456290).to_h
+    @account[:containers] = @google_api.list_account_containers(account_id: @account[:account_id])
+    @account[:user_permissions] = @google_api.list_account_user_permissions(account_id: @account[:account_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users
     resources :organizations
+    root to: "site#index"
   end
   get "example", to: "site#example", as: :example
   root to: "site#index"
