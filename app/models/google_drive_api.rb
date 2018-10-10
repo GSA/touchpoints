1a752b194eb23704b3c914bf4d876f178c1502aa
Ryan Wold <ryan.wold@gsa.gov>
2018-10-10 13:52:21 -0700

    check in GoogleDriveApi

---
+++ app/models/google_drive_api.rb
@@ -0,0 +1,59 @@
+class GoogleDriveApi
+  require 'google/apis/drive_v3'
+
+  attr_reader :service
+
+  def initialize
+    Google::Apis.logger.level = Logger::DEBUG if Rails.env.development?
+
+    scope = [
+      "https://www.googleapis.com/auth/drive",
+      "https://www.googleapis.com/auth/drive.file"
+    ].freeze
+
+    authorizer = Google::Auth::ServiceAccountCredentials.make_creds({
+      json_key_io: File.open('tmp/google_service_account.json'),
+      scope: scope
+    })
+    authorizer.fetch_access_token!
+
+    @service = Google::Apis::DriveV3::DriveService.new
+    @service.authorization = authorizer
+  end
+
+  def list_files
+    Google::Apis::DriveV3::FileList.new
+  end
+
+  def add_permissions(file_id:)
+    callback = lambda do |res, err|
+      if err
+        # Handle error...
+        puts err.body
+      else
+        puts "Permission ID: #{res.id}"
+      end
+    end
+
+    user_permission = {
+      type: 'user',
+      role: 'writer',
+      email_address: 'ryanjwold@gmail.com'
+    }
+    @service.create_permission(file_id,
+                              user_permission,
+                              fields: 'id',
+                              &callback)
+
+    user_permission = {
+      type: 'user',
+      role: 'owner',
+      email_address: 'ryanjwold@gmail.com'
+    }
+    @service.create_permission(file_id,
+                              user_permission,
+                              transfer_ownership: true,
+                              fields: 'id',
+                              &callback)
+  end
+end
