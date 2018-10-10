7446997e2e3a4349f74be24b5d64e9bfbc9255ed
Ryan Wold <ryan.wold@gsa.gov>
2018-10-10 10:53:49 -0700

    spec the /status page

--- spec/controllers/site_controller_spec.rb
+++ spec/controllers/site_controller_spec.rb
@@ -5,4 +5,23 @@ describe SiteController, type: :controller do
     get :index
     expect(response).to be_successful
   end
+
+  it "should get example" do
+    get :example
+    expect(response).to be_successful
+  end
+
+  describe "get status" do
+    before do
+      get :status
+    end
+
+    it "should be successful" do
+      expect(response).to be_successful
+    end
+
+    it "should be a Hash" do
+      expect(JSON.parse(response.body)).to be_kind_of(Hash)
+    end
+  end
 end
