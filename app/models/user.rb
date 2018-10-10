09c6889dcf16536646523f6e05a3f8bb3bdb6dd0
Ryan Wold <ryan.wold@gsa.gov>
2018-10-10 15:59:36 -0700

    fix User Sign Up for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ApplicationRecord
 
   belongs_to :organization, optional: true
   has_many :touchpoints, through: :organization
+
+  # TODO - remove this overriding behavior that disabled email sending
+  #        once Touchpoints has an email account and config setup
+  def send_confirmation_notification?
+    # Explicitly confirm the User's account
+    self.confirm
+
+    return false
+  end
 end
