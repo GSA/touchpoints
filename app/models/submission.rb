63f55fdde0514050eeb9687faa0baa66fc054eab
Ryan Wold <ryan.wold@gsa.gov>
2018-10-12 08:44:26 -0700

    very basic validation of the a11's first field

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -3,6 +3,7 @@ class Submission < ApplicationRecord
 
   validates :first_name, presence: true, if: :validate_first_name?
   validates :body, presence: true, if: :validate_body?
+  validates :overall_satisfaction, presence: true, if: :validate_overall_satisfaction?
 
   # NOTE: this is brittle.
   #       this pattern will require every field to declare its validations
@@ -13,5 +14,9 @@ class Submission < ApplicationRecord
 
   def validate_body?
     self.touchpoint.form.kind == "open-ended"
+  end
+
+  def validate_overall_satisfaction?
+    self.touchpoint.form.kind == "a11"
   end
 end
