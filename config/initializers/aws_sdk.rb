6d8a763e2b84718fe9e20b63df64c84965d6eea4
Ryan Wold <ryan.wold@gsa.gov>
2019-02-05 15:47:00 -0800

    don't initialize the mailer during test

--- config/initializers/aws_sdk.rb
+++ config/initializers/aws_sdk.rb
@@ -1,2 +1,3 @@
+return unless [:staging, :production].include?(Rails.env)
 creds = Aws::Credentials.new(ENV.fetch("AWS_ACCESS_KEY_ID"), ENV.fetch("AWS_SECRET_ACCESS_KEY"))
 Aws::Rails.add_action_mailer_delivery_method(:aws_sdk, credentials: creds, region: 'us-east-1')
