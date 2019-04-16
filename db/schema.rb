0ecdce9ec0fff47380ee1a076744062575f14d3f
Ryan Wold <ryan.wold@gsa.gov>
2019-04-16 11:17:56 -0700

    add domain to Organization
    to auto-associate a User to an Organization, by domain

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_183051) do
+ActiveRecord::Schema.define(version: 2019_04_16_181427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2019_03_26_183051) do
     t.string "abbreviation"
     t.text "notes"
     t.integer "external_id"
+    t.string "domain"
   end
 
   create_table "programs", force: :cascade do |t|
