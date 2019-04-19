1b7a0d137b79fe7fb2619c17f864aed6cbd609e5
Ryan Wold <ryan.wold@gsa.gov>
2019-04-19 15:55:30 -0700

    fix: use ENV GOOGLE_TAG_MANAGER_ACCOUNT_ID

--- app/controllers/admin/site_controller.rb
+++ app/controllers/admin/site_controller.rb
@@ -1,9 +1,9 @@
 class Admin::SiteController < AdminController
   before_action :ensure_admin
-  
+
   def index
     @google_api = GoogleApi.new
-    @account = @google_api.get_account(account_id: 4172456290).to_h
+    @account = @google_api.get_account(account_id: ENV.fetch('GOOGLE_TAG_MANAGER_ACCOUNT_ID')).to_h
     @account[:containers] = @google_api.list_account_containers(account_id: @account[:account_id])
     @account[:user_permissions] = @google_api.list_account_user_permissions(account_id: @account[:account_id])
   end
