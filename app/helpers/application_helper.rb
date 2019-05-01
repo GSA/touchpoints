888baab7a66584b944e12b48312947ce35207d3c
Ryan Wold <ryan.wold@gsa.gov>
2019-04-30 22:13:50 -0700

    remove text
    * suppress specific flash messages globally and show them when adding a user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def suppress_main_layout_flash?
+    if flash && ["User successfully added", "User successfully removed"].include?(flash.notice)
+      return true
+    end
+  end
 end
