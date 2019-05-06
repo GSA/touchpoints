dcbf3fed042a661c83b881b3c7c27d2a4c2d80bb
Ryan Wold <ryan.wold@gsa.gov>
2019-05-06 14:40:32 -0700

    remove old migrations
    rake db:schema:load instead

--- db/migrate/20190226213307_add_expiration_date_to_touchpoint.rb
+++
@@ -1,5 +0,0 @@
-class AddExpirationDateToTouchpoint < ActiveRecord::Migration[5.2]
-  def change
-    add_column :touchpoints, :expiration_date, :date
-  end
-end
--- db/migrate/20190416181427_add_organization_domain.rb
+++
@@ -1,5 +0,0 @@
-class AddOrganizationDomain < ActiveRecord::Migration[5.2]
-  def change
-    add_column :organizations, :domain, :string
-  end
-end
