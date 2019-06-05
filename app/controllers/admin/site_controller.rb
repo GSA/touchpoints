8401c3fdb8a4733b1361dc6862462dabcd036efa
Ryan Wold <ryan.wold@gsa.gov>
2019-06-04 23:43:34 -0700

    de-couple from GTM

--- app/controllers/admin/site_controller.rb
+++ app/controllers/admin/site_controller.rb
@@ -2,9 +2,5 @@ class Admin::SiteController < AdminController
   before_action :ensure_admin
 
   def index
-    @google_api_service = GoogleApi.new
-    @account = @google_api_service.get_account(account_id: ENV.fetch('GOOGLE_TAG_MANAGER_ACCOUNT_ID')).to_h
-    @account[:containers] = @google_api_service.list_account_containers(account_id: @account[:account_id])
-    @account[:user_permissions] = @google_api_service.list_account_user_permissions(account_id: @account[:account_id])
   end
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,21 +1,10 @@
 class Service < ApplicationRecord
   belongs_to :organization
-  has_one :container
   has_many :touchpoints
   has_many :user_services
   has_many :users, through: :user_services
 
   validates :name, presence: true
-
-  def create_container!
-    return unless Rails.env.test?
-    return unless self.container
-
-    Container.create!({
-      service: self,
-      name: "#{self.name} Container",
-    })
-  end
 
   def user_role?(user:)
     user_service = self.user_services.find_by_user_id(user.id)
