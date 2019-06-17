82b7a1ccf0517048e43ec2036e3401a3905549f6
Ryan Wold <ryan.wold@gsa.gov>
2019-06-17 11:07:36 -0700

    show a blank Form to preview when viewing a FormTemplate

--- spec/features/admin/form_templates_spec.rb
+++ spec/features/admin/form_templates_spec.rb
@@ -6,18 +6,23 @@ feature "Managing Form Templates", js: true do
   let!(:form_template) { FactoryBot.create(:form_template) }
 
   context "as Admin" do
-    before "user creates a Touchpoint" do
+    before do
       login_as admin
-      visit admin_form_templates_path
     end
 
-    describe "create new Form Template" do
-      before do
-        click_link "New Form Template"
+    describe "new Form Template" do
+      before "user creates a Form Template" do
+        visit admin_form_templates_path
       end
 
-      it "arrive at Form" do
-        expect(page).to have_content("New Form Template")
+      describe "create new Form Template" do
+        before do
+          click_link "New Form Template"
+        end
+
+        it "arrive at Form" do
+          expect(page).to have_content("New Form Template")
+        end
       end
     end
 
@@ -28,6 +33,33 @@ feature "Managing Form Templates", js: true do
     describe "display Edit link" do
       it "display Edit link" do
         expect(page).to have_link("Edit")
+      end
+    end
+
+    describe "viewing each Form Template" do
+      before do
+        @a11 = FactoryBot.create(:form_template, :a11)
+        @recruiter = FactoryBot.create(:form_template, :recruiter)
+        @oewci = FactoryBot.create(:form_template, :open_ended_with_contact_info)
+        visit admin_form_templates_path
+      end
+
+      it "loads each of the forms" do
+        visit admin_form_template_path(FormTemplate.first)
+        expect(page).to have_content("Viewing a Form Template")
+        expect(page).to have_content("Name: #{FormTemplate.first.name}")
+
+        visit admin_form_template_path(@a11)
+        expect(page).to have_content("Viewing a Form Template")
+        expect(page).to have_content("Name: #{@a11.name}")
+
+        visit admin_form_template_path(@recruiter)
+        expect(page).to have_content("Viewing a Form Template")
+        expect(page).to have_content("Name: #{@recruiter.name}")
+
+        visit admin_form_template_path(@oewci)
+        expect(page).to have_content("Viewing a Form Template")
+        expect(page).to have_content("Name: #{@oewci.name}")
       end
     end
   end
