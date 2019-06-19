ff31e15ff9965d5dee7e765e8a4147eefb100073
Ryan Wold <ryan.wold@gsa.gov>
2019-06-19 09:19:00 -0700

    Service Manager can flag a Service as a HISP
    * High Impact Service Provider

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_233620) do
+ActiveRecord::Schema.define(version: 2019_06_19_161124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 2019_06_17_233620) do
     t.string "service_manager"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "hisp", default: false
   end
 
   create_table "submissions", force: :cascade do |t|
