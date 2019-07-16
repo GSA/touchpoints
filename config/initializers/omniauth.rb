79d3fbfd7fa6eb3762b083173417d70315503ab3
Ryan Wold <ryan.wold@gsa.gov>
2019-07-16 12:52:38 -0700

    read cert from env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     client_id: ENV.fetch("LOGIN_GOV_CLIENT_ID"),
     idp_base_url: ENV.fetch("LOGIN_GOV_IDP_BASE_URL"),
     ial: 1,
-    private_key: OpenSSL::PKey::RSA.new(File.read("config/#{ENV.fetch("LOGIN_GOV_PRIVATE_KEY")}.pem")),
+    private_key: OpenSSL::PKey::RSA.new(ENV.fetch("LOGIN_GOV_PRIVATE_KEY")),
     redirect_uri: ENV.fetch("LOGIN_GOV_REDIRECT_URI")
   }
 end
