94ac7790201a4612897d05ebe0ab798c752afb19
Ryan Wold <ryan.wold@gsa.gov>
2019-07-23 12:11:59 -0700

    fix forms without a touchpoint

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,5 +1,5 @@
 class Form < ApplicationRecord
-  has_one :touchpoint
+  has_one :touchpoint, required: true
 
   validates :name, presence: true
   validates :character_limit, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100000 }
