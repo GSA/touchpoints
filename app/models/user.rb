a131d7cfa8fd9bcd9145bc1bb86b2c1a4e7dcca0
Ryan Wold <ryan.wold@gsa.gov>
2019-08-28 13:49:49 -0700

    remove TP specific login
    * in favor of Login.gov
    * move inline styles to CSS

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :lockable, and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+  # :lockable
+  devise :database_authenticatable,
+         :rememberable,
          :trackable,
-         # :confirmable,
          :timeoutable
 
   devise :omniauthable, omniauth_providers: [:login_dot_gov]
@@ -31,7 +30,7 @@ class User < ApplicationRecord
     # For login.gov native accounts
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+      user.password = Devise.friendly_token[0,24]
     end
   end
 
