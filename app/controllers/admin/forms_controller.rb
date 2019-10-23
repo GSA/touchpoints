b12409765baf94ad8542084904ec4a8947c985e8
Ryan Wold <ryan.wold@gsa.gov>
2019-10-23 15:42:22 -0700

    create the first Form Section along with the Form

--- app/controllers/admin/forms_controller.rb
+++ app/controllers/admin/forms_controller.rb
@@ -23,7 +23,6 @@ class Admin::FormsController < AdminController
   def create
     @form = Form.new(form_params)
     @form.kind = "custom"
-    @form.form_sections.new(title: "Page 1", position: 1)
 
     respond_to do |format|
       if @form.save
--- app/models/form.rb
+++ app/models/form.rb
@@ -7,6 +7,12 @@ class Form < ApplicationRecord
   validates :character_limit, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100000 }
   validates_length_of :disclaimer_text, in: 0..500, allow_blank: true
 
+  after_create :create_first_form_section
+
+  def create_first_form_section
+    self.form_sections.create(title: "Page 1", position: 1)
+  end
+
   def success_text
     super.present? ? super : "Thank you. Your feedback has been received."
   end
