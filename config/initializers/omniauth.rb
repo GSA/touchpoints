b5f26dae0ee23a7e3d44c2761b6bead916e5d103
Ryan Wold <ryan.wold@gsa.gov>
2019-11-04 11:46:07 -0800

    start to make Login.gov an opt-in configuration

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,13 +1,19 @@
+# The ENV variable "LOGIN_GOV_ENABLED" must be set to `true`
+# in order to enable Login.gov integration
+#
 # LOGIN_GOV_PRIVATE_KEY expects the .pem in a certain format
 # For the .pem (private key):
 #
 # pem_string = File.open("your-filepath.pem", "r").read
 # use the value `pem_string`
 
-# eg:
+# Example:
 # export LOGIN_GOV_PRIVATE_KEY="#{pem_string}"
-# Of note: .gsub() is used for the `private_key`
-# below, to decode the line breaks properly
+#
+# Of note: .gsub() is used below for the `private_key`,
+# to decode the line breaks properly
+
+return unless ENV.fetch("LOGIN_GOV_ENABLED") == "true"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :login_dot_gov, {
