56682c7d61f088b72c516135c60cf9fb37c5dbea
Ryan Wold <ryan.wold@gsa.gov>
2019-11-25 15:02:37 -0800

    add character_limit for Question
    only show to text fields and textareas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_203238) do
+ActiveRecord::Schema.define(version: 2019_11_25_225809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 2019_11_25_203238) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "form_section_id"
+    t.integer "character_limit"
   end
 
   create_table "services", force: :cascade do |t|
