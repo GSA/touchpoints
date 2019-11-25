b15fcc6086eb5822ad07fffe452f31f78574ad16
Ryan Wold <ryan.wold@gsa.gov>
2019-11-25 14:50:05 -0800

    add QuestionOption.value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_230820) do
+ActiveRecord::Schema.define(version: 2019_11_25_203238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_230820) do
     t.integer "position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "value"
   end
 
   create_table "questions", force: :cascade do |t|
