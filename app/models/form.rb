123512d70def5c1cdb44ff19fbd902cd6c9bebf4
Ryan Wold <ryan.wold@gsa.gov>
2019-11-26 16:50:03 -0800

    fix Form copy

--- app/models/form.rb
+++ app/models/form.rb
@@ -21,4 +21,38 @@ class Form < ApplicationRecord
   def create_first_form_section
     self.form_sections.create(title: (I18n.t 'form.page_1'), position: 1)
   end
+
+  def duplicate!
+    new_form = self.dup
+    new_form.name = "Copy of #{self.name}"
+    new_form.save
+
+    # Manually remove the Form Section created with create_first_form_section
+    new_form.form_sections.destroy_all
+
+    # Loop Form Sections to create them for new_form
+    self.form_sections.each do |section|
+      new_form_section = section.dup
+      new_form_section.form = new_form
+      new_form_section.save
+
+      # Loop Questions to create them for new_form and new_form_sections
+      section.questions.each do |question|
+        new_question = question.dup
+        new_question.form = new_form
+        new_question.form_section = new_form_section
+        new_question.save
+
+        # Loop Questions to create them for Questions
+        question.question_options.each do |option|
+          new_question_option = option.dup
+          new_question_option.question = new_question
+          new_question_option.save
+        end
+      end
+    end
+
+    return new_form
+  end
+
 end
