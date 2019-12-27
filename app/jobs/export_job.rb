6f243c784971c5ccafc0ed18ce4a47c2a7e3b9ba
Allen Taylor <akt@frikshun.com>
2019-12-27 12:39:24 -0500

    TP-446 Remove 10 second sleep in export job, which was added to test disabled button during export

--- app/jobs/export_job.rb
+++ app/jobs/export_job.rb
@@ -2,9 +2,6 @@ class ExportJob < ApplicationJob
   queue_as :default
 
   def perform(uuid,touchpoint_id,filename = "export.csv")
-    # Do something later
-
-    sleep 10
     csv_content = Touchpoint.find(touchpoint_id).to_csv
     ActionCable.server.broadcast(
       "exports_channel_#{uuid}", csv: csv_content, filename: filename
