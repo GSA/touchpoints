46cf0d7b2f03b4afcead4872cf5beb2899c1f0e4
Allen Taylor <akt@frikshun.com>
2020-02-19 18:43:25 -0500

    Add schedule for checking expiring touchpoints

---
+++ config/schedule.rb
@@ -0,0 +1,10 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+set :output, "tmp/cron_log.log"
+
+every 1.days do
+  runner "ScheduledTask.check_expiring_forms"
+end
