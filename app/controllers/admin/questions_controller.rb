ca4a69a7a8942532155745e5edd25f0607cc90fe
Prabha <prabha@Prabhas-MacBook-Pro.local>
2020-04-24 19:29:49 -0400

    WIP version of ajax rendering

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,10 +1,7 @@
 class Admin::QuestionsController < AdminController
   before_action :set_form, only: [:new, :create, :show, :edit, :update, :destroy]
+  before_action :all_questions, only: [:index, :create, :update]
   before_action :set_question, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @questions = Question.all
-  end
 
   def show
   end
@@ -24,7 +21,8 @@ class Admin::QuestionsController < AdminController
     respond_to do |format|
       if @question.save
         format.html { redirect_to edit_admin_form_path(@form), notice: 'Question was successfully created.' }
-        format.json { render :show, status: :created, location: @question }
+        format.js { render :layout => false }
+        format.json { render :index, status: :created, location: @question }
       else
         format.html { render :new }
         format.json { render json: @question.errors, status: :unprocessable_entity }
@@ -35,7 +33,8 @@ class Admin::QuestionsController < AdminController
   def update
     respond_to do |format|
       if @question.update(question_params)
-        format.html { redirect_to edit_admin_form_path(@form), notice: 'Question was successfully updated.' }
+        format.html { redirect_to edit_admin_form_question_path(@form,question), notice: 'Question was successfully updated.' }
+        format.js { render :layout => false }
         format.json {
           render :show, status: :ok, location: @question
         }
@@ -51,12 +50,17 @@ class Admin::QuestionsController < AdminController
 
     @question.destroy
     respond_to do |format|
-      format.html { redirect_to edit_admin_form_url(@form), notice: 'Question was successfully destroyed.' }
+      format.html { redirect_to edit_admin_form_url, notice: 'Question was successfully destroyed.' }
+      format.js { render :layout => false }
       format.json { head :no_content }
     end
   end
 
   private
+    def all_questions
+      @questions = Question.all
+    end  
+
     def set_question
       @question = Question.find(params[:id])
     end
