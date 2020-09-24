c784e9f6b4b9f8753a621ab5820892474d70fe16
Ryan Wold <ryan.wold@gsa.gov>
2020-09-24 15:20:22 -0700

    refactor view calls to controller

--- app/controllers/admin/site_controller.rb
+++ app/controllers/admin/site_controller.rb
@@ -2,6 +2,11 @@ class Admin::SiteController < AdminController
   def index
     @forms = Form.non_templates
     @response_groups = Submission.group("date(created_at)").size.sort.last(45)
+
+    @days_since = params[:recent] && params[:recent].to_i <= 30 ? params[:recent].to_i : 3
+    todays_submissions = Submission.where("created_at > ?", Time.now - @days_since.days)
+    form_ids = todays_submissions.collect(&:form_id).uniq
+    @recent_forms = Form.find(form_ids)
   end
 
   def events
