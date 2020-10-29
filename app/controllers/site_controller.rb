c184cc9b36e511477353e31852bd5a467db26c6b
Ryan Wold <ryan.wold@gsa.gov>
2020-10-29 12:36:25 -0700

    ensure /index remains valid

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,11 +1,7 @@
 class SiteController < ApplicationController
   def index
     if current_user
-      if ENV["INDEX_URL"].present?
-        redirect_to ENV["INDEX_URL"]
-      else
-        redirect_to admin_root_path
-      end
+      redirect_to admin_root_path
     end
   end
 
