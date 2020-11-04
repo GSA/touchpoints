3855180e12fed5534aed7c6193fe038730788ecb
Ryan Wold <ryan.wold@gsa.gov>
2020-11-04 14:03:44 -0800

    move /lib to /app/lib, which gets autoloaded

--- config/application.rb
+++ config/application.rb
@@ -72,8 +72,6 @@ module Touchpoints
 
     config.active_job.queue_adapter = :sidekiq
 
-    config.autoload_paths << Rails.root.join("lib")
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
