e95647fee840feb06ec60ac29cfaa9f0efba5b22
Ryan Wold <ryan.wold@gsa.gov>
2021-03-30 21:19:06 -0700

    scope ServiceStages to its Service

--- app/controllers/admin/service_stages_controller.rb
+++ app/controllers/admin/service_stages_controller.rb
@@ -1,58 +1,55 @@
 class Admin::ServiceStagesController < AdminController
   before_action :ensure_admin
+  before_action :set_service
   before_action :set_service_stage, only: [:show, :edit, :update, :destroy]
 
-  # GET /service_stages
   def index
-    @service_stages = ServiceStage.all
+    @service_stages = @service.service_stages
   end
 
-  # GET /service_stages/1
   def show
   end
 
-  # GET /service_stages/new
   def new
     @service_stage = ServiceStage.new
   end
 
-  # GET /service_stages/1/edit
   def edit
   end
 
-  # POST /service_stages
   def create
     @service_stage = ServiceStage.new(service_stage_params)
 
     if @service_stage.save
-      redirect_to admin_service_stage_path(@service_stage), notice: 'Service stage was successfully created.'
+      redirect_to admin_service_service_stage_path(@service, @service_stage), notice: 'Service stage was successfully created.'
     else
       render :new
     end
   end
 
-  # PATCH/PUT /service_stages/1
   def update
     if @service_stage.update(service_stage_params)
-      redirect_to admin_service_stage_path(@service_stage), notice: 'Service stage was successfully updated.'
+      redirect_to admin_service_service_stage_path(@service, @service_stage), notice: 'Service stage was successfully updated.'
     else
       render :edit
     end
   end
 
-  # DELETE /service_stages/1
   def destroy
     @service_stage.destroy
-    redirect_to admin_service_stages_url, notice: 'Service stage was successfully destroyed.'
+    redirect_to admin_service_service_stages_url(@service), notice: 'Service stage was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
+    def set_service
+      @service = Service.find(params[:service_id])
+    end
+
     def set_service_stage
       @service_stage = ServiceStage.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def service_stage_params
       params.require(:service_stage).permit(:name, :description, :service_id, :notes, :time, :total_eligble_population)
     end
