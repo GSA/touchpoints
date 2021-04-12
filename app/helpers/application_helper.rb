89608d7ca3297790bac51076a6b69432d0bf210a
Allen Taylor <allen.taylor@uniteus.com>
2021-04-12 14:43:53 -0400

    TP-1005 Added support for text_phone_field

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,6 +14,8 @@ module ApplicationHelper
       "form.querySelector(\"##{question.answer_field}\") && form.querySelector(\"##{question.answer_field}\").value"
     elsif question.question_type == "text_email_field"
       "form.querySelector(\"##{question.answer_field}\") && form.querySelector(\"##{question.answer_field}\").value"
+    elsif question.question_type == "text_phone_field"
+      "form.querySelector(\"##{question.answer_field}\") && form.querySelector(\"##{question.answer_field}\").value"      
     elsif question.question_type == "textarea"
       "form.querySelector(\"##{question.answer_field}\") && form.querySelector(\"##{question.answer_field}\").value"
     elsif question.question_type == "radio_buttons"
