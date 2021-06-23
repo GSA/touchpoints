683bd53be7d41cc195bcbca42788daaadd9b2466
Ryan Wold <ryan.wold@gsa.gov>
2021-06-22 21:52:53 -0700

    sort collections by org.name, year, quarter

--- app/controllers/admin/collections_controller.rb
+++ app/controllers/admin/collections_controller.rb
@@ -6,9 +6,9 @@ class Admin::CollectionsController < AdminController
 
   def index
     if admin_permissions?
-      @collections = Collection.all.includes(:organization).order('organizations.name')
+      @collections = Collection.all.order('organizations.name', :year, :quarter).includes(:organization)
     else
-      @collections = current_user.organization.collections.includes(:organization)
+      @collections = current_user.organization.collections.order('organizations.name', :year, :quarter).includes(:organization)
     end
   end
 
