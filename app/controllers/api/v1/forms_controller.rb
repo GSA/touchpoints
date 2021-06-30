96397fb4175cf26769d6088320e087f36759d84b
Allen Taylor <akt@frikshun.com>
2021-06-30 14:16:51 -0400

    WIP

--- app/controllers/api/v1/forms_controller.rb
+++ app/controllers/api/v1/forms_controller.rb
@@ -9,11 +9,14 @@ class Api::V1::FormsController < ::ApiController
 
   def show
     @form = current_user.forms.find_by_short_uuid(params[:id])
+    @page_num = (params[:page_num].present? ? params[:page_num].to_i : 0)
+    @page_size = (params[:page_size].present? ? params[:page_size].to_i : 500)
+    @page_size = 5000 if @page_size > 5000
 
     respond_to do |format|
       format.json {
         if @form
-          render json: @form, include: [:questions, :submissions], serializer: FullFormSerializer
+          render json: @form, include: [:questions, :submissions], serializer: FullFormSerializer, page_num: @page_num, page_size: @page_size
         else
           render json: { error: { message: "no form with Short UUID of #{params[:id]}", status: 404 } }, status: 404
         end
