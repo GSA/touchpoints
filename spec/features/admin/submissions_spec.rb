c05e3f1bbd45b5694f704ad5b36b4a5204c26002
Ryan Wold <64987852+ryanwoldatwork@users.noreply.github.com>
2021-07-14 18:14:23 -0700

    reorder columns in submissions display table
    * show View, Archive, Flag on the left
    * set a max-width on table response fields
    * update "form" text to "survey"

--- spec/features/admin/submissions_spec.rb
+++ spec/features/admin/submissions_spec.rb
@@ -196,13 +196,13 @@ feature "Submissions", js: true do
                 expect(page).to_not have_content("<a>")
                 expect(page).to_not have_content("</a>")
                 expect(page).to have_content("A textarea field")
-                expect(page).to have_content("Created At")
-                expect(page).to have_content("Status")
 
-                expect(page).to have_content("received")
                 expect(page).to have_link("View")
                 expect(page).to have_link("Flag")
                 expect(page).to have_link("Archive")
+                expect(page).to have_content("Status")
+                expect(page).to have_content("received")
+                expect(page).to have_content("Created At")
               end
             end
           end
