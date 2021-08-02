4d06a37de151380016c8094e64f4033e638bf8cc
Ryan Wold <ryan.wold@gsa.gov>
2021-08-02 16:43:00 -0700

    show Service Providers per Organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,6 +2,7 @@ class Organization < ApplicationRecord
   has_many :users
   has_many :services
   has_many :forms
+  has_many :service_providers
   has_many :collections
 
   mount_uploader :logo, LogoUploader
