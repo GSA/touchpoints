5c2af43d8f3456357304de6150f0d4fc782470d5
Ryan Wold <ryan.wold@gsa.gov>
2021-08-11 17:08:38 -0700

    add ServiceProvider.inactive
    * to exclude inactive HISPs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_234411) do
+ActiveRecord::Schema.define(version: 2021_08_12_000141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -257,6 +257,7 @@ ActiveRecord::Schema.define(version: 2021_08_11_234411) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "new"
+    t.boolean "inactive"
   end
 
   create_table "service_stage_barriers", force: :cascade do |t|
