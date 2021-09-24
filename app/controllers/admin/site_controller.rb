4ea7450e07a1d5b2ef35d82d4c7444ac25e31d1e
Allen Taylor <akt@frikshun.com>
2021-09-24 10:06:17 -0400

    TP-1266 Make all charts work off the same period; add 60/90 days options

--- app/controllers/admin/site_controller.rb
+++ app/controllers/admin/site_controller.rb
@@ -1,10 +1,10 @@
 class Admin::SiteController < AdminController
   def index
     @forms = Form.non_templates
-    @response_groups = Submission.group("date(created_at)").size.sort.last(45)
-    @user_groups = User.group("date(created_at)").size.sort.last(45)
 
-    @days_since = params[:recent] && params[:recent].to_i <= 30 ? params[:recent].to_i : 3
+    @days_since = params[:recent] && params[:recent].to_i <= 90 ? params[:recent].to_i : 3
+    @response_groups = Submission.group("date(created_at)").size.sort.last(@days_since.days)
+    @user_groups = User.group("date(created_at)").size.sort.last(@days_since.days)
     todays_submissions = Submission.where("created_at > ?", Time.now - @days_since.days)
     form_ids = todays_submissions.collect(&:form_id).uniq
     @recent_forms = Form.find(form_ids)
