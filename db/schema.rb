72ed9d2f25287c195b862cde813900c63192bd85
Ryan Wold <ryan.wold@gsa.gov>
2021-12-23 14:51:44 -0800

    add Performance Manager user role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_184803) do
+ActiveRecord::Schema.define(version: 2021_12_23_223113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -485,6 +485,8 @@ ActiveRecord::Schema.define(version: 2021_12_23_184803) do
     t.string "api_key"
     t.datetime "api_key_updated_at"
     t.boolean "organizational_website_manager", default: false
+    t.boolean "performance_manager", default: false
+    t.boolean "registry_manager", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
