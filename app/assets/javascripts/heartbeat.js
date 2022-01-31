bdb7990c956b371fbedd36eae2abf0152e47a1dc
Allen Taylor <akt@frikshun.com>
2022-01-31 17:03:30 -0500

    TP-1372 review feedback

--- app/assets/javascripts/heartbeat.js
+++ app/assets/javascripts/heartbeat.js
@@ -3,11 +3,7 @@ window.sendHeartbeat = function(url) {
   $('input,textarea,select,radio').on('blur',function(e){
       $.ajax({
         type: "GET",
-        url: url,
-        success: function (data) {
-          // console.log(data);
-        }
+        url: url
       });
-
   });
 }
