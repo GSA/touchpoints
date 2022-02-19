b5c873199b1a2d6018dcda6ed73bbe5a2fcf16de
Ryan Wold <ryan.wold@gsa.gov>
2022-02-18 19:31:52 -0800

    order service providers api
    * helps ease things for performance.gov

--- app/controllers/api/v1/service_providers_controller.rb
+++ app/controllers/api/v1/service_providers_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ServiceProvidersController < ::ApiController
   def index
     respond_to do |format|
       format.json {
-        render json: ServiceProvider.all, each_serializer: ServiceProviderSerializer
+        render json: ServiceProvider.all.includes(:organization).order("organizations.name", :name), each_serializer: ServiceProviderSerializer
       }
     end
   end
