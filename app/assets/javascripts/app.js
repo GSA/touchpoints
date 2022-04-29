3dd5a9151b09267ea34747e12747cdc8187933bf
Allen Taylor <akt@frikshun.com>
2022-04-29 13:35:58 -0400

    resolve js conflicts

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,39 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require cable
+//= require file-saver/dist/FileSaver
+//= require jquery3
+//= require jquery-ui
+//= require rails-ujs
+//= require uswds-init
+//= require heartbeat
+
+function generateUUID() {
+  var S4 = function() {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  };
+  return (
+    S4() +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    S4() +
+    S4()
+  );
+}
