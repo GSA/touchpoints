b17257e3ee03536d16432b76da82382cecaf6fe3
Allen Taylor <akt@frikshun.com>
2022-04-29 11:49:07 -0400

    Remove all traces of webpacker

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello from Stimulus!"
+    console.log("Stimulus Hello World!")
   }
 }
