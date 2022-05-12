fc1bf0ee16afef84199bee665e6941f9936e9bd8
Ryan Wold <ryan.wold@gsa.gov>
2022-05-12 16:17:15 -0700

    add digital service account name

--- app/serializers/digital_service_account_serializer.rb
+++ app/serializers/digital_service_account_serializer.rb
@@ -1,3 +1,17 @@
 class DigitalServiceAccountSerializer < ActiveModel::Serializer
-  attributes :id, :organization_id, :user_id, :service, :service_url, :account, :language, :status, :short_description, :long_description, :tags
+  attributes
+    :id,
+    :name,
+    :organization_id,
+    :organization_name,
+    :organization_abbreviation,
+    :user_id,
+    :service,
+    :service_url,
+    :account,
+    :language,
+    :status,
+    :short_description,
+    :long_description,
+    :tags
 end
