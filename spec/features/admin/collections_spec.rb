8191c4eb4cbc8e3e13b03db52c26e8d320e1cb8e
Ryan Wold <ryan.wold@gsa.gov>
2022-05-31 16:32:30 -0700

    restore

--- spec/features/admin/collections_spec.rb
+++ spec/features/admin/collections_spec.rb
@@ -103,6 +103,8 @@ feature "Data Collections", js: true do
         end
 
         it "creates a new Collection" do
+          expect(page).to have_content("ABOUT THIS DATA COLLECTION")
+          expect(page).to have_content("CX Quarterly Reporting")
           expect(page).to have_content("Collection was successfully created.")
         end
       end
