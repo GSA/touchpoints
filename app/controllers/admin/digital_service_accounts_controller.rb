a6e12f1512c39276cef4b6db24104363346daecf
Ryan Wold <ryan.wold@gsa.gov>
2022-06-15 15:05:06 -0700

    scope the label style override to #show pages

--- app/controllers/admin/digital_service_accounts_controller.rb
+++ app/controllers/admin/digital_service_accounts_controller.rb
@@ -31,10 +31,10 @@ module Admin
 
   def create
     @digital_service_account = DigitalServiceAccount.new(digital_service_account_params)
+    @digital_service_account.organization_list.add(current_user.organization_id)
 
     if @digital_service_account.save
       current_user.add_role(:contact, @digital_service_account)
-      @digital_service_account.organization_list.add(current_user.organization.id)
 
       Event.log_event(Event.names[:digital_service_account_created], "Digital Service Account", @digital_service_account.id, "Digital Service Account #{@digital_service_account.name} created at #{DateTime.now}", current_user.id)
 
