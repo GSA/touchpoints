5b0e665be2d209a704663bc028bfc0125b68d4c7
Ryan Wold <ryan.wold@gsa.gov>
2022-06-15 18:11:58 -0700

    differentiate permissions between user contacts and registry managers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -164,15 +164,15 @@ class ApplicationController < ActionController::Base
     return false unless user.present?
     return true if registry_manager_permissions?(user: current_user)
 
-    digital_service_account.user == user
+    user.has_role?(:contact, digital_service_account)
   end
 
   helper_method :digital_product_permissions?
   def digital_product_permissions?(digital_product:, user:)
     return false unless user.present?
-    return true if registry_manager_permissions?(user: current_user)
+    return true if registry_manager_permissions?(user: user)
 
-    digital_product.user == user
+    user.has_role?(:contact, digital_product)
   end
 
   helper_method :service_permissions?
