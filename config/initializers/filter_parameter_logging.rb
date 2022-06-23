6bed9eae6070730a8b1163d27b8b3e46603e780c
Allen Taylor <akt@frikshun.com>
2022-06-23 13:44:59 -0400

    cop

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [
-  :password,
-  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
+Rails.application.config.filter_parameters += %i[
+  password
+  passw secret token _key crypt salt certificate otp ssn
 ]
