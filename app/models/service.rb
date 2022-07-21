76f282a6d8c1a0fa8e8c45f3b23189e9f42fa281
Ryan Wold <ryan.wold@gsa.gov>
2022-07-21 11:27:16 -0700

    update rails
    * rename counter cache column service.service_stages_count
    * add counter cache for Form.questions_count
    * make service provider optional for a service

--- app/models/service.rb
+++ app/models/service.rb
@@ -8,7 +8,7 @@ class Service < ApplicationRecord
   has_paper_trail
 
   belongs_to :organization
-  belongs_to :service_provider
+  belongs_to :service_provider, optional: true
   has_many :service_stages
   has_many :omb_cx_reporting_collections
   has_many :collections, through: :omb_cx_reporting_collections
