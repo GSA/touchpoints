63081ee457ba4a8f2f9550de7d3551fdb686645e
Ryan Wold <ryan.wold@gsa.gov>
2022-11-18 16:33:59 -0800

    log searches for analytics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_18_020341) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_18_235123) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -451,6 +451,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_18_020341) do
     t.string "placeholder_text"
     t.string "help_text"
     t.index ["form_id"], name: "index_questions_on_form_id"
+  end
+
+  create_table "registry_searches", force: :cascade do |t|
+    t.string "agency"
+    t.string "keywords"
+    t.string "platform"
+    t.string "status"
+    t.string "session_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "roles", force: :cascade do |t|
