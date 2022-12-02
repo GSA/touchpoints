d62b0091742d7cf51bbaffc2e2927106f3367b13
Ryan Wold <ryan.wold@gsa.gov>
2022-12-01 16:25:28 -0800

    move IVN model specs

--- spec/models/ivn_source_spec.rb
+++ spec/models/ivn_source_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Admin::IvnComponent, type: :model do
+RSpec.describe IvnSource, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
