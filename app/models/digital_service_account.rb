25989ab35777dbad0f0679eddf134ea41c0f058e
Ryan Wold <ryan.wold@gsa.gov>
2022-12-12 14:56:19 -0800

    export registry entries to .csv

--- app/models/digital_service_account.rb
+++ app/models/digital_service_account.rb
@@ -83,4 +83,89 @@ class DigitalServiceAccount < ApplicationRecord
     ]
   end
 
+  def user_email
+    return nil unless self.user_id
+    if user
+      return user.email
+    end
+  end
+
+  def contact_emails
+    contacts.collect(&:email).join(", ")
+  end
+
+  def organization_names
+    Organization.find(self.organization_list).collect(&:name).join(", ")
+  end
+
+  def self.to_csv
+    header_attributes = DigitalServiceAccount.first.attributes.keys
+
+    digital_service_accounts = DigitalServiceAccount.all
+
+    attributes = [
+      :id,
+      :account,
+      :name,
+      :user_email,
+      :contact_emails,
+      :organization_names,
+      :tag_list,
+      :service,
+      :service_url,
+      :language,
+      :short_description,
+      :long_description,
+      :notes,
+      :certified_at,
+      :created_at,
+      :updated_at,
+      :aasm_state,
+      :legacy_id,
+      :legacy_notes,
+    ]
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      digital_service_accounts.each do |digital_service_account|
+        csv << attributes.map { |attr| digital_service_account.send(attr) }
+      end
+    end
+  end
+
+  def self.list2
+    {
+      'Disqus' => "https://disqus.com/",
+      'Eventbrite' => '',
+      'Facebook' => '',
+      'Flickr' => '',
+      'Foursquare' => '',
+      'Giphy' => '',
+      'Github' => '',
+      'Google plus' => '',
+      'Ideascale' => '',
+      'Instagram' => '',
+      'Linkedin' => '',
+      'Livestream' => '',
+      'Mastodon' => '',
+      'Medium' => '',
+      'Myspace' => '',
+      'Pinterest' => '',
+      'Reddit' => '',
+      'Scribd' => '',
+      'Slideshare' => '',
+      'Socrata' => '',
+      'Storify' => '',
+      'Tumblr' => '',
+      'Twitter' => '',
+      'Uservoice' => '',
+      'Ustream' => '',
+      'Vimeo' => '',
+      'Yelp' => '',
+      'Youtube' => '',
+      'Other' => nil
+    }
+  end
+
 end
