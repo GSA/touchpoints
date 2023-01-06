b104ad139659d840de2b7542861f446bde52bb07
Ryan Wold <ryan.wold@gsa.gov>
2023-01-05 16:26:48 -0800

    ensure response summary works for A-11 forms without question 6 or 7

--- spec/factories/submission.rb
+++ spec/factories/submission.rb
@@ -4,5 +4,17 @@ FactoryBot.define do
   factory :submission do
     answer_01 { 'submission response body text' }
     form
+
+    trait :a11 do
+      answer_01 { '1' }
+      answer_02 { '2' }
+      answer_03 { '3' }
+      answer_04 { '4' }
+      answer_05 { '5' }
+      answer_06 { '4' }
+      answer_07 { '3' }
+      answer_08 { 'text 1' }
+      answer_09 { 'text 2' }
+    end
   end
 end
