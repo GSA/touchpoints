9f79d410d8bfe7dc3ca5abeb56d278aa6c651335
Ryan Wold <ryan.wold@gsa.gov>
2023-02-07 14:43:07 -0800

    add CSCRM Manager permissions

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -144,6 +144,7 @@ module Admin
         :performance_manager,
         :service_manager,
         :registry_manager,
+        :cscrm_data_collection_manager,
         :email,
         :first_name,
         :last_name,
