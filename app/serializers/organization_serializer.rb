b54a2b3414114217e5c593bb79defa192c851879
Ryan Wold <ryan.wold@gsa.gov>
2023-03-22 15:19:15 -0700

    index submissions by created_at

--- app/serializers/organization_serializer.rb
+++ app/serializers/organization_serializer.rb
@@ -13,5 +13,5 @@ class OrganizationSerializer < ActiveModel::Serializer
              :performance_url,
              :strategic_plan_url,
              :learning_agenda_url,
-             :cfo_act_agency,
+             :cfo_act_agency
 end
