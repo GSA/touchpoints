2e96aa1aacd4ae7b310c0a86119ce1896f7f789c
Ryan Wold <ryan.wold@gsa.gov>
2023-09-27 21:09:20 -0700

    default spec

--- spec/requests/admin/cx_action_plans_spec.rb
+++ spec/requests/admin/cx_action_plans_spec.rb
@@ -12,8 +12,8 @@ require 'rails_helper'
 # of tools you can use to make these specs even more expressive, but we're
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
-RSpec.describe "/cx_action_plans", type: :request do
-  
+RSpec.describe "/admin/cx_action_plans", type: :request do
+
   # This should return the minimal set of attributes required to create a valid
   # CxActionPlan. As you add validations to CxActionPlan, be sure to
   # adjust the attributes here as well.
@@ -28,7 +28,7 @@ RSpec.describe "/cx_action_plans", type: :request do
   describe "GET /index" do
     it "renders a successful response" do
       CxActionPlan.create! valid_attributes
-      get cx_action_plans_url
+      get admin_cx_action_plans_url
       expect(response).to be_successful
     end
   end
@@ -36,14 +36,20 @@ RSpec.describe "/cx_action_plans", type: :request do
   describe "GET /show" do
     it "renders a successful response" do
       cx_action_plan = CxActionPlan.create! valid_attributes
-      get cx_action_plan_url(cx_action_plan)
+      get admin_cx_action_plan_url(cx_action_plan)
       expect(response).to be_successful
     end
   end
 
   describe "GET /new" do
+    let(:user) { FactoryBot.create(:user, :admin) }
+
+    before do
+      sign_in(user)
+    end
+
     it "renders a successful response" do
-      get new_cx_action_plan_url
+      get new_admin_cx_action_plan_url
       expect(response).to be_successful
     end
   end
@@ -51,7 +57,7 @@ RSpec.describe "/cx_action_plans", type: :request do
   describe "GET /edit" do
     it "renders a successful response" do
       cx_action_plan = CxActionPlan.create! valid_attributes
-      get edit_cx_action_plan_url(cx_action_plan)
+      get edit_admin_cx_action_plan_url(cx_action_plan)
       expect(response).to be_successful
     end
   end
@@ -60,12 +66,12 @@ RSpec.describe "/cx_action_plans", type: :request do
     context "with valid parameters" do
       it "creates a new CxActionPlan" do
         expect {
-          post cx_action_plans_url, params: { cx_action_plan: valid_attributes }
+          post admin_cx_action_plans_url, params: { cx_action_plan: valid_attributes }
         }.to change(CxActionPlan, :count).by(1)
       end
 
       it "redirects to the created cx_action_plan" do
-        post cx_action_plans_url, params: { cx_action_plan: valid_attributes }
+        post admin_cx_action_plans_url, params: { cx_action_plan: valid_attributes }
         expect(response).to redirect_to(cx_action_plan_url(CxActionPlan.last))
       end
     end
@@ -73,16 +79,16 @@ RSpec.describe "/cx_action_plans", type: :request do
     context "with invalid parameters" do
       it "does not create a new CxActionPlan" do
         expect {
-          post cx_action_plans_url, params: { cx_action_plan: invalid_attributes }
+          post admin_cx_action_plans_url, params: { cx_action_plan: invalid_attributes }
         }.to change(CxActionPlan, :count).by(0)
       end
 
-    
+
       it "renders a response with 422 status (i.e. to display the 'new' template)" do
-        post cx_action_plans_url, params: { cx_action_plan: invalid_attributes }
+        post admin_cx_action_plans_url, params: { cx_action_plan: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)
       end
-    
+
     end
   end
 
@@ -94,27 +100,27 @@ RSpec.describe "/cx_action_plans", type: :request do
 
       it "updates the requested cx_action_plan" do
         cx_action_plan = CxActionPlan.create! valid_attributes
-        patch cx_action_plan_url(cx_action_plan), params: { cx_action_plan: new_attributes }
+        patch admin_cx_action_plan_url(cx_action_plan), params: { cx_action_plan: new_attributes }
         cx_action_plan.reload
         skip("Add assertions for updated state")
       end
 
       it "redirects to the cx_action_plan" do
         cx_action_plan = CxActionPlan.create! valid_attributes
-        patch cx_action_plan_url(cx_action_plan), params: { cx_action_plan: new_attributes }
+        patch admin_cx_action_plan_url(cx_action_plan), params: { cx_action_plan: new_attributes }
         cx_action_plan.reload
         expect(response).to redirect_to(cx_action_plan_url(cx_action_plan))
       end
     end
 
     context "with invalid parameters" do
-    
+
       it "renders a response with 422 status (i.e. to display the 'edit' template)" do
         cx_action_plan = CxActionPlan.create! valid_attributes
         patch cx_action_plan_url(cx_action_plan), params: { cx_action_plan: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)
       end
-    
+
     end
   end
 
