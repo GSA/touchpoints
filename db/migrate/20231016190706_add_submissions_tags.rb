6aecde9639a5f301dbc505a68363f4971d10062d
Ryan Wold <ryan.wold@gsa.gov>
2023-10-16 18:31:57 -0700

    update apart from a deploy/migration

--- db/migrate/20231016190706_add_submissions_tags.rb
+++ db/migrate/20231016190706_add_submissions_tags.rb
@@ -1,14 +1,5 @@
 class AddSubmissionsTags < ActiveRecord::Migration[7.0]
   def change
     add_column :submissions, :submission_tags, :string, array: true, default: []
-
-    Submission.in_batches do |group|
-        group.each do |submission|
-            unless tags = submission.tag_list
-              submission.update(submission_tags: tags)
-            end
-        end; nil
-    end; nil
-
   end
 end
