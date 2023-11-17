5d261b30984296e930c701ab85092003ed25b480
Ryan Wold <ryan.wold@gsa.gov>
2023-11-16 20:53:14 -0800

    upload csv

--- app/controllers/admin/cx_collection_details_controller.rb
+++ app/controllers/admin/cx_collection_details_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CxCollectionDetailsController < AdminController
-  before_action :set_cx_collection_detail, only: %i[ show edit upload update destroy ]
+  before_action :set_cx_collection_detail, only: %i[ show edit upload upload_csv update destroy ]
   before_action :set_cx_collections, only: %i[ new edit upload ]
 
   def index
@@ -54,6 +54,27 @@ class Admin::CxCollectionDetailsController < AdminController
       format.html { redirect_to cx_collection_details_url, notice: "Cx collection detail was successfully destroyed." }
       format.json { head :no_content }
     end
+  end
+
+  # Handle a large-ish csv upload (5+ MB) to S3
+  def upload_csv
+    file = params[:file] # Assuming the file comes from a form field named 'file'
+    bucket = ENV.fetch("S3_UPLOADS_AWS_BUCKET_NAME")
+
+    s3 = Aws::S3::Resource.new
+    key = "cx_data_collections/cx-upload-#{Time.now.to_i}-#{file.original_filename}}"
+    obj = s3.bucket(bucket).object(file.original_filename)
+    # Upload the file
+    response = obj.upload_file(file.path)
+
+    CxCollectionDetailUpload.create!({
+      user_id: current_user.id,
+      cx_collection_detail_id: @cx_collection_detail.id,
+      size: obj.size,
+      key: obj.key,
+    })
+
+    render :upload, message: 'File successfully uploaded'
   end
 
   private
