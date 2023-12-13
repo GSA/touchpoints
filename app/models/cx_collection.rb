76a00e505538085454c445bcfa3ae7923230d1f5
Ryan Wold <ryan.wold@gsa.gov>
2023-12-13 11:24:32 -0800

    cx collections updates

--- app/models/cx_collection.rb
+++ app/models/cx_collection.rb
@@ -42,4 +42,64 @@ class CxCollection < ApplicationRecord
       transitions to: :draft
     end
   end
+
+  def self.to_csv
+    collections = CxCollection.all.order(:fiscal_year, :quarter, 'organizations.name').includes(:organization)
+
+    attributes = %i[
+      id
+      name
+      start_date
+      end_date
+      service_provider_id
+      service_provider_name
+      service_provider_organization_id
+      service_provider_organization_name
+      service_provider_organization_abbreviation
+      organization_id
+      organization_name
+      organization_abbreviation
+      user_email
+      fiscal_year
+      quarter
+      reflection
+      created_at
+      updated_at
+      rating
+      aasm_state
+      integrity_hash
+      omb_cx_reporting_collections_count
+    ]
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      collections.each do |collection|
+        csv << attributes = [
+          collection.id,
+          collection.name,
+          collection.start_date,
+          collection.end_date,
+          collection.service_provider_id,
+          collection.service_provider.name,
+          collection.service_provider.organization_id,
+          collection.service_provider.organization_name,
+          collection.service_provider.organization_abbreviation,
+          collection.organization_id,
+          collection.organization.name,
+          collection.organization.abbreviation,
+          collection.user.email,
+          collection.fiscal_year,
+          collection.quarter,
+          collection.reflection,
+          collection.created_at,
+          collection.updated_at,
+          collection.rating,
+          collection.aasm_state,
+          collection.integrity_hash,
+          collection.cx_collection_details.size,
+        ]
+      end
+    end
+  end
 end
