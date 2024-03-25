d696b010daf348d614bdc27f546d4abec46d7383
Ryan Wold <ryan.wold@gsa.gov>
2024-03-25 14:47:42 -0700

    response_count already exists

--- db/migrate/20240325185142_add_forms_submissions_count.rb
+++
@@ -1,9 +0,0 @@
-class AddFormsSubmissionsCount < ActiveRecord::Migration[7.1]
-  def change
-    add_column :forms, :submissions_count, :integer, default: 0
-
-    Form.all.each do |form|
-      form.update_attribute(:submissions_count, form.submissions.count)
-    end
-  end
-end
