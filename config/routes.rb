d5cf433aaab7b35ee9e24ad44f0dbea9958fd9fa
Ryan Wold <ryan.wold@gsa.gov>
2024-04-08 12:43:29 -0700

    remove PRA document generation

--- config/routes.rb
+++ config/routes.rb
@@ -317,7 +317,6 @@ Rails.application.routes.draw do
         get 'notifications', to: 'forms#notifications', as: :notifications
         get 'example', to: 'forms#example', as: :example
         get 'export', to: 'forms#export', as: :export
-        get 'export_pra_document', as: :export_pra_document
         get 'export_submissions', to: 'forms#export_submissions', as: :export_submissions
         get 'export_a11_v2_submissions', to: 'forms#export_a11_v2_submissions', as: :export_a11_v2_submissions
         get 'export_a11_header', to: 'forms#export_a11_header', as: :export_a11_header
