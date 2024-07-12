4efd1e28ce71db5b28e1ef5bc724d7e4318a56e5
Ryan Wold <ryan.wold@gsa.gov>
2024-07-11 18:28:16 -0700

    separate web server and worker job deploys

--- .circleci/deploy-sidekiq.sh
+++ .circleci/deploy-sidekiq.sh
@@ -9,8 +9,8 @@ then
   echo "Logging into cloud.gov"
   # Log into CF and push
   cf login -a $CF_API_ENDPOINT -u $CF_PRODUCTION_SPACE_DEPLOYER_USERNAME -p $CF_PRODUCTION_SPACE_DEPLOYER_PASSWORD -o $CF_ORG -s prod
-  echo "PUSHING web servers to Production..."
-  cf push touchpoints --strategy rolling
+  echo "PUSHING to PRODUCTION..."
+  cf push touchpoints-production-sidekiq-worker --strategy rolling
   echo "Push to Production Complete."
 else
   echo "Not on the production branch."
@@ -21,8 +21,8 @@ then
   echo "Logging into cloud.gov"
   # Log into CF and push
   cf login -a $CF_API_ENDPOINT -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE
-  echo "Pushing web servers to Demo..."
-  cf push touchpoints-demo --strategy rolling
+  echo "Pushing to Demo..."
+  cf push touchpoints-demo-sidekiq-worker --strategy rolling
   echo "Push to Demo Complete."
 else
   echo "Not on the main branch."
@@ -33,8 +33,8 @@ then
   echo "Logging into cloud.gov"
   # Log into CF and push
   cf login -a $CF_API_ENDPOINT -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE
-  echo "Pushing web servers to Staging..."
-  cf push touchpoints-staging --strategy rolling
+  echo "Pushing to Staging..."
+  cf push touchpoints-staging-sidekiq-worker --strategy rolling
   echo "Push to Staging Complete."
 else
   echo "Not on the develop branch."
