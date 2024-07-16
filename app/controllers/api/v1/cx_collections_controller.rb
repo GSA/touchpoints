34a1830ba8ee380b2ca8c425be9b55b731cac743
Ryan Wold <ryan.wold@gsa.gov>
2024-07-16 13:40:09 -0700

    return all websites

--- app/controllers/api/v1/cx_collections_controller.rb
+++ app/controllers/api/v1/cx_collections_controller.rb
@@ -6,7 +6,7 @@ module Api
       def index
         respond_to do |format|
           format.json do
-            if params[:all]
+            if params[:all].present? && params[:all].to_s == '1'
               render json: CxCollection.all.order(:id), each_serializer: CxCollectionSerializer
             else
               render json: CxCollection.published.order(:id), each_serializer: CxCollectionSerializer
