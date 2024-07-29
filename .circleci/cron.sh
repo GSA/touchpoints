c5e5229d21a8a9e952762796f9cf5dcb8c11ae39
Ryan Wold <ryan.wold@gsa.gov>
2024-07-29 13:50:39 -0700

    remove whenever

--- .circleci/cron.sh
+++ .circleci/cron.sh
@@ -9,13 +9,24 @@ echo "Logging into cloud.gov"
 cf login -a $CF_API_ENDPOINT -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE
 echo "Running tasks in Staging..."
 
-cf run-task touchpoints-staging-sidekiq-worker -c "rake scheduled_jobs:archive_surveys"
+#
+# === Staging environment =========================================================
+#
+
+# Users
+cf run-task touchpoints-staging-sidekiq-worker -c "rake scheduled_jobs:send_one_week_until_inactivation_warning"
 cf run-task touchpoints-staging-sidekiq-worker -c "rake scheduled_jobs:send_two_weeks_until_inactivation_warning"
 cf run-task touchpoints-staging-sidekiq-worker -c "rake scheduled_jobs:deactivate_inactive_users"
+
+# Forms
 cf run-task touchpoints-staging-sidekiq-worker -c "rake scheduled_jobs:send_daily_notifications"
 cf run-task touchpoints-staging-sidekiq-worker -c "rake scheduled_jobs:send_weekly_notifications"
-# email for forms without activity for (x) days
+cf run-task touchpoints-staging-sidekiq-worker -c "rake scheduled_jobs:check_expiring_forms"
+cf run-task touchpoints-staging-sidekiq-worker -c "rake scheduled_jobs:archive_forms"
 
+#
+# === Demo environment =========================================================
+#
 echo "Running tasks in Demo..."
 cf run-task touchpoints-demo-sidekiq-worker -c "rake scheduled_jobs:archive_surveys"
 cf run-task touchpoints-demo-sidekiq-worker -c "rake scheduled_jobs:send_two_weeks_until_inactivation_warning"
