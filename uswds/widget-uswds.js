a8fee276f9278ed3eab91373ede333f5f0493413
Ryan Wold <ryan.wold@gsa.gov>
2024-07-30 15:00:48 -0700

    add uswds datepicker js and css

--- uswds/widget-uswds.js
+++ uswds/widget-uswds.js
@@ -1,4 +1,6 @@
 const modal = require("@uswds/uswds/packages/usa-modal/src/index.js");
+const DatePicker = require("@uswds/uswds/packages/usa-date-picker/src/index.js");
 
 // Initialize modal event listeners
 modal.on();
+DatePicker.on();
