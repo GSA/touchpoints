f620742f88ca5914f00b3a4cca533bd8f4972f46
Ryan Wold <64987852+ryanwoldatwork@users.noreply.github.com>
2024-07-31 14:26:45 -0700

    update model docs
    * add combobox support in the embedded modal

--- uswds/widget-uswds.js
+++ uswds/widget-uswds.js
@@ -1,4 +1,6 @@
 const modal = require("@uswds/uswds/packages/usa-modal/src/index.js");
+const ComboBox = require("@uswds/uswds/packages/usa-combo-box/src/index.js");
 
 // Initialize modal event listeners
 modal.on();
+ComboBox.on();
