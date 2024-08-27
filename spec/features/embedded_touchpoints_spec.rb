866c1ec47bd7f8203078793a0c98c76810826b42
Ryan Wold <ryan.wold@gsa.gov>
2024-08-27 15:12:53 -0700

    next

--- spec/features/embedded_touchpoints_spec.rb
+++ spec/features/embedded_touchpoints_spec.rb
@@ -49,7 +49,9 @@ feature 'Touchpoints', js: true do
             fill_in("#{form.ordered_questions[5].ui_selector}_other", with: 'other 3')
 
             select('Option 2', from: form.ordered_questions[6].ui_selector)
-            find('a', text: "Next").click
+            within(".pagination-buttons") do
+              click_link("Next")
+            end
             expect(page).to have_content('Custom elements')
             find('.submit_form_button').click
 
