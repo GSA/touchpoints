a382c195f9d5d90b80c17cd1c5bac5ea0d2f4a12
Ryan Wold <ryan.wold@gsa.gov>
2024-09-03 09:30:31 -0700

    just whitespace

--- spec/features/embedded_touchpoints_spec.rb
+++ spec/features/embedded_touchpoints_spec.rb
@@ -29,53 +29,53 @@ feature 'Touchpoints', js: true do
             expect(page).to have_content('Do you have a few minutes to help us test this site?')
 
             within(".fba-modal") do
-            expect(page).to have_content('Page 1')
-            expect(page).to have_no_content('Option elements')
-            expect(page).to have_no_content('Custom elements')
-            fill_in form.ordered_questions.first.ui_selector, with: 'input field'
-            fill_in form.ordered_questions.second.ui_selector, with: 'email'
-            fill_in form.ordered_questions.third.ui_selector, with: 'textarea'
-            expect(page).to have_link('html')
+              expect(page).to have_content('Page 1')
+              expect(page).to have_no_content('Option elements')
+              expect(page).to have_no_content('Custom elements')
+              fill_in form.ordered_questions.first.ui_selector, with: 'input field'
+              fill_in form.ordered_questions.second.ui_selector, with: 'email'
+              fill_in form.ordered_questions.third.ui_selector, with: 'textarea'
+              expect(page).to have_link('html')
 
-            find(".pagination-buttons.text-right", visible: true).click_link("Next")
-            expect(page).to have_content('Please enter a valid value')
-            expect(page).to have_content('This is help text')
-            fill_in form.ordered_questions.second.ui_selector, with: 'email@example.gov'
+              find(".pagination-buttons.text-right", visible: true).click_link("Next")
+              expect(page).to have_content('Please enter a valid value')
+              expect(page).to have_content('This is help text')
+              fill_in form.ordered_questions.second.ui_selector, with: 'email@example.gov'
 
-            find(".pagination-buttons.text-right", visible: true).click_link("Next")
+              find(".pagination-buttons.text-right", visible: true).click_link("Next")
 
-            expect(page).to have_content('Option elements')
-            expect(page).to have_no_content('Page 1')
-            expect(page).to have_no_content('Custom elements')
-            expect(all("#question_#{form.ordered_questions[4].id} .usa-radio__label").size).to eq(3)
-            all("#question_#{form.ordered_questions[4].id} .usa-radio__label").last.click
-            fill_in("#{form.ordered_questions[4].ui_selector}_other", with: 'otro 2')
+              expect(page).to have_content('Option elements')
+              expect(page).to have_no_content('Page 1')
+              expect(page).to have_no_content('Custom elements')
+              expect(all("#question_#{form.ordered_questions[4].id} .usa-radio__label").size).to eq(3)
+              all("#question_#{form.ordered_questions[4].id} .usa-radio__label").last.click
+              fill_in("#{form.ordered_questions[4].ui_selector}_other", with: 'otro 2')
 
-            expect(page).to have_content("Custom Question Checkboxes")
-            expect(page).to have_content("This is help text for checkboxes")
-            all('.usa-checkbox__label').each(&:click)
-            expect(page).to have_content("Enter other text")
+              expect(page).to have_content("Custom Question Checkboxes")
+              expect(page).to have_content("This is help text for checkboxes")
+              all('.usa-checkbox__label').each(&:click)
+              expect(page).to have_content("Enter other text")
 
-            expect(page).to have_css("##{form.ordered_questions[5].ui_selector}_other")
-            fill_in("#{form.ordered_questions[5].ui_selector}_other", with: 'other 3')
-            find("##{form.ordered_questions[5].ui_selector}_other", visible: true).native.send_key :tab
+              expect(page).to have_css("##{form.ordered_questions[5].ui_selector}_other")
+              fill_in("#{form.ordered_questions[5].ui_selector}_other", with: 'other 3')
+              find("##{form.ordered_questions[5].ui_selector}_other", visible: true).native.send_key :tab
 
-            expect(page).to have_content("This is help text for a dropdown.")
-            select('Option 2', from: form.ordered_questions[6].ui_selector)
-            find(".pagination-buttons.text-right", visible: true).click_link("Next")
-            expect(page).to have_content('Custom elements')
-            expect(page).to have_no_content('Page 1')
-            expect(page).to have_no_content('Option elements')
-            find('.submit_form_button').click
+              expect(page).to have_content("This is help text for a dropdown.")
+              select('Option 2', from: form.ordered_questions[6].ui_selector)
+              find(".pagination-buttons.text-right", visible: true).click_link("Next")
+              expect(page).to have_content('Custom elements')
+              expect(page).to have_no_content('Page 1')
+              expect(page).to have_no_content('Option elements')
+              find('.submit_form_button').click
 
-            # shows success flash message
-            expect(page).to have_content('Success')
-            expect(page).to have_content('Thank you. Your feedback has been received.')
+              # shows success flash message
+              expect(page).to have_content('Success')
+              expect(page).to have_content('Thank you. Your feedback has been received.')
 
-            # doesn't reset form; leave the flash message
-            find(".fba-modal-close").click
-            find("#fba-button").click
-            expect(page).to have_content('Thank you. Your feedback has been received.')
+              # doesn't reset form; leave the flash message
+              find(".fba-modal-close").click
+              find("#fba-button").click
+              expect(page).to have_content('Thank you. Your feedback has been received.')
             end
           end
 
