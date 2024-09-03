0cf4fffc38a575839acf04a2a73bdc92fe273e17
Ryan Wold <ryan.wold@gsa.gov>
2024-09-03 09:30:13 -0700

    tighten spec

--- spec/features/embedded_touchpoints_spec.rb
+++ spec/features/embedded_touchpoints_spec.rb
@@ -22,19 +22,22 @@ feature 'Touchpoints', js: true do
 
         context 'default success text' do
           before do
+            expect(page).to have_content("main content")
             click_on('Help improve this site') # opens modal
 
             expect(page).to have_content('Help improve this site')
             expect(page).to have_content('Do you have a few minutes to help us test this site?')
 
+            within(".fba-modal") do
             expect(page).to have_content('Page 1')
             expect(page).to have_no_content('Option elements')
             expect(page).to have_no_content('Custom elements')
             fill_in form.ordered_questions.first.ui_selector, with: 'input field'
             fill_in form.ordered_questions.second.ui_selector, with: 'email'
             fill_in form.ordered_questions.third.ui_selector, with: 'textarea'
+            expect(page).to have_link('html')
 
-            click_on 'Next'
+            find(".pagination-buttons.text-right", visible: true).click_link("Next")
             expect(page).to have_content('Please enter a valid value')
             expect(page).to have_content('This is help text')
             fill_in form.ordered_questions.second.ui_selector, with: 'email@example.gov'
@@ -55,7 +58,7 @@ feature 'Touchpoints', js: true do
 
             expect(page).to have_css("##{form.ordered_questions[5].ui_selector}_other")
             fill_in("#{form.ordered_questions[5].ui_selector}_other", with: 'other 3')
-            find("##{form.ordered_questions[5].ui_selector}_other").native.send_key :tab
+            find("##{form.ordered_questions[5].ui_selector}_other", visible: true).native.send_key :tab
 
             expect(page).to have_content("This is help text for a dropdown.")
             select('Option 2', from: form.ordered_questions[6].ui_selector)
@@ -73,6 +76,7 @@ feature 'Touchpoints', js: true do
             find(".fba-modal-close").click
             find("#fba-button").click
             expect(page).to have_content('Thank you. Your feedback has been received.')
+            end
           end
 
           it 'renders success flash message' do
