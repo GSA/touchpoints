8a985cdf85e8539d1002bddecb8d7159adbf7709
Ryan Wold <ryan.wold@gsa.gov>
2024-09-09 15:31:42 -0700

    use the ?
    https://guides.rubyonrails.org/active_record_querying.html#conditions

--- app/models/digital_service_account.rb
+++ app/models/digital_service_account.rb
@@ -23,11 +23,13 @@ class DigitalServiceAccount < ApplicationRecord
   scope :filtered_accounts, lambda { |query, organization_abbreviation, aasm_state, account|
     @organization = Organization.find_by_abbreviation(organization_abbreviation)
 
+    wildcard_query = "%#{query}%"
+
     items = all
     items = items.tagged_with(@organization.id, context: 'organizations') if @organization.present?
     items = items.where(aasm_state:) if aasm_state.present? && aasm_state.downcase != 'all'
     items = items.where(service: account) if account.present? && account.downcase != 'all'
-    items = items.where("name ILIKE '%#{query}%' OR account ILIKE '%#{query}%' OR short_description ILIKE '%#{query}%' OR service_url ILIKE '%#{query}%'") if query && query.length >= 3
+    items = items.where("name ILIKE ? OR account ILIKE ? OR short_description ILIKE ? OR service_url ILIKE ?", wildcard_query, wildcard_query, wildcard_query, wildcard_query) if query && query.length >= 3
 
     items
   }
