3a7394de4292981201dca5da920bc7242eb3c67b
Shelley Nason <shelley.nason@gsa.gov>
2024-10-21 17:47:23 -0500

    Build widget JS bundle for browser-only instead of using UMD

--- uswds/widget-uswds.js
+++ uswds/widget-uswds.js
@@ -2,7 +2,8 @@ const ComboBox = require("@uswds/uswds/packages/usa-combo-box/src/index.js");
 const DatePicker = require("@uswds/uswds/packages/usa-date-picker/src/index.js");
 const Modal = require("@uswds/uswds/packages/usa-modal/src/index.js");
 
-module.exports = {
+// Make these components available in the global browser scope so that Touchpoints code can call initialization functions.
+window.fbaUswds = {
   ComboBox,
   DatePicker,
   Modal
