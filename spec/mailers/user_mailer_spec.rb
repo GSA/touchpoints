6403b0957f066c3b1f44d86132e31f2965d070ef
Ryan Wold <ryan.wold@gsa.gov>
2024-10-25 09:48:01 -0700

    update email

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -97,7 +97,7 @@ RSpec.describe UserMailer, type: :mailer do
   describe 'quarterly_performance_update' do
     let!(:organization) { FactoryBot.create(:organization) }
     let!(:user) { FactoryBot.create(:user, organization:) }
-    let!(:collection) { FactoryBot.create(:collection, user:) }
+    let!(:cx_collection) { FactoryBot.create(:cx_collection, user:) }
     let(:mail) { UserMailer.quarterly_performance_notification(collection_id: collection.id) }
 
     before do
@@ -105,8 +105,8 @@ RSpec.describe UserMailer, type: :mailer do
     end
 
     it 'renders the headers' do
-      expect(mail.subject).to eq("Quarterly Performance Data Collection Ready: #{collection.name}")
-      expect(mail.to).to eq([collection.user.email])
+      expect(mail.subject).to eq("Quarterly Performance Data Collection Ready: #{cx_collection.name}")
+      expect(mail.to).to eq([cx_collection.user.email])
       expect(mail.from).to eq([ENV.fetch('TOUCHPOINTS_EMAIL_SENDER')])
     end
 
