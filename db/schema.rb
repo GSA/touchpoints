46635d9f010cf34313b9cf7ff2621796d2ae3c0d
Ryan Wold <64987852+ryanwoldatwork@users.noreply.github.com>
2025-02-06 09:20:15 -0800

    remove Offerings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_30_195535) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_06_005216) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -348,13 +348,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_30_195535) do
     t.text "description"
     t.string "url"
     t.integer "organization_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "offerings", force: :cascade do |t|
-    t.string "name"
-    t.integer "service_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
