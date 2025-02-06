c54f2c137e994d154e387c6f95e639dabd2632b9
Ryan Wold <ryan.wold@gsa.gov>
2025-02-06 09:00:23 -0800

    append: updated schema timestamp
    so migration runs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_30_195535) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_06_005216) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
