6b33b8ee6e105fdf2555d56175cfa13946fc0e62
Ryan Wold <ryan.wold@gsa.gov>
2025-02-18 12:22:11 -0800

    test adapter

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -21,6 +21,8 @@ Rails.application.configure do
   # Configure public file server for tests with cache-control for performance.
   config.public_file_server.headers = { "cache-control" => "public, max-age=3600" }
 
+  config.active_job.queue_adapter = :test
+
   # Show full error reports.
   config.consider_all_requests_local = true
   config.cache_store = :null_store
