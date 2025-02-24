0d62e841b393248722277f2ef9a6b0ceb827a683
Ryan Wold <64987852+ryanwoldatwork@users.noreply.github.com>
2025-02-24 14:38:14 -0800

    remove IVN
    * remove Submission updates in migration. this will be run manually, once.

--- app/serializers/admin/ivn_source_component_link_serializer.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Admin
-  class IvnSourceComponentLinkSerializer < ActiveModel::Serializer
-    attributes :id, :from_id, :to_id
-  end
-end
--- app/serializers/ivn_component_serializer.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class IvnComponentSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :url
-end
