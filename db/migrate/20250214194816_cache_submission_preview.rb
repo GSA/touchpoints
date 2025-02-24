cdb2ee6fa3816c8fa045ae104bf07af70dbd6b0d
Ryan Wold <ryan.wold@gsa.gov>
2025-02-24 13:17:00 -0800

    remove migration to run manually

--- db/migrate/20250214194816_cache_submission_preview.rb
+++ db/migrate/20250214194816_cache_submission_preview.rb
@@ -1,11 +1,5 @@
 class CacheSubmissionPreview < ActiveRecord::Migration[7.2]
   def change
     add_column :submissions, :preview, :string, default: ""
-
-    Submission.unscoped.in_batches(of: 10_000) do |batch|
-      batch.each do |submission|
-        submission.set_preview
-      end
-    end
   end
 end
