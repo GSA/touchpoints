5a1771656fe91e9bb9f971ae5d14bd3c374f0ebd
Riley Seaburg <riley.seaburg@gsa.gov>
2025-11-20 12:00:12 -0600

    Add robust path detection for widget_renderer library

--- ext/widget_renderer/lib/widget_renderer.rb
+++ ext/widget_renderer/lib/widget_renderer.rb
@@ -3,5 +3,18 @@
 require 'rutie'
 
 module WidgetRenderer
-  Rutie.new(:widget_renderer).init 'Init_widget_renderer', File.expand_path('..', __dir__)
+  root = File.expand_path('..', __dir__)
+
+  # Attempt to find the library path
+  # Standard structure: root/target/release
+  if File.directory?(File.join(root, 'target', 'release'))
+    path = File.join(root, 'target', 'release')
+  # Fallback if root is one level up (e.g. ext/)
+  elsif File.directory?(File.join(root, 'widget_renderer', 'target', 'release'))
+    path = File.join(root, 'widget_renderer', 'target', 'release')
+  else
+    path = root
+  end
+
+  Rutie.new(:widget_renderer).init 'Init_widget_renderer', path
 end
