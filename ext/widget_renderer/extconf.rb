82c1c344027ec423502891447ae64c8ec0559c26
Riley Seaburg <riley.seaburg@gsa.gov>
2025-11-20 16:58:58 -0600

    Add ldd debug to extconf.rb

--- ext/widget_renderer/extconf.rb
+++ ext/widget_renderer/extconf.rb
@@ -1,10 +1,25 @@
 require 'mkmf'
 
-unless system('which rustc > /dev/null 2>&1')
-  abort "Rust compiler not found. Please install Rust."
-end
+abort 'Rust compiler not found. Please install Rust.' unless system('which rustc > /dev/null 2>&1')
 
-system('cargo build --release') or abort "Failed to build Rust extension"
+system('cargo build --release') or abort 'Failed to build Rust extension'
 
-$LDFLAGS += " -L#{Dir.pwd}/target/release -lwidget_renderer"
+# Debug: Check dependencies of the generated library
+puts "Checking dependencies of libwidget_renderer.so..."
+system('ldd target/release/libwidget_renderer.so')
+puts "End of dependencies check."
+
+# Check for the library in the local target directory or the workspace target directory
+local_target = File.join(Dir.pwd, 'target', 'release')
+workspace_target = File.expand_path('../../target/release', Dir.pwd)
+
+lib_dir = if Dir.glob(File.join(local_target, 'libwidget_renderer.{so,dylib,dll}')).any?
+            local_target
+          elsif Dir.glob(File.join(workspace_target, 'libwidget_renderer.{so,dylib,dll}')).any?
+            workspace_target
+          else
+            local_target # Fallback
+          end
+
+$LDFLAGS += " -L#{lib_dir} -lwidget_renderer"
 create_makefile('widget_renderer')
