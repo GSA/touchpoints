d59854881a745bb624a5ea6636db222610794da1
Riley Seaburg <riley.seaburg@gsa.gov>
2025-11-21 22:29:11 -0600

    Load widget renderer from lib/widget_renderer

--- config/initializers/widget_renderer.rb
+++ config/initializers/widget_renderer.rb
@@ -1,7 +1,7 @@
 # Load the Rust widget renderer extension
 begin
   # Try loading from the extension directory
-  require_relative '../../ext/widget_renderer/widget_renderer'
+  require_relative '../../ext/widget_renderer/lib/widget_renderer'
 rescue LoadError => e
   Rails.logger.warn "Widget renderer extension not available: #{e.message}"
   # Attempt to build the Rust extension on the fly (installs Rust via extconf if needed)
@@ -11,7 +11,7 @@ rescue LoadError => e
     Dir.chdir(ext_dir) do
       system('ruby extconf.rb') && system('make')
     end
-    require_relative '../../ext/widget_renderer/widget_renderer'
+    require_relative '../../ext/widget_renderer/lib/widget_renderer'
     Rails.logger.info 'Successfully compiled widget_renderer extension at runtime.'
   rescue StandardError => build_error
     Rails.logger.warn "Widget renderer build failed: #{build_error.class}: #{build_error.message}"
