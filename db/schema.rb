2a919afa87248f316c1f29a063a76ec2059f5120
Riley Seaburg <riley.seaburg@gsa.gov>
2025-12-10 14:16:25 -0600

    Update schema version to include new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_17_034402) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_10_192727) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
